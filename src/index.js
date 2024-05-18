0452a65a9d22d0f1ca5968cf9175d79a439cec81
Pradeep Kumar <pra45516@gmail.com>
2024-05-18 12:32:54 +0530

    Project setup is done

---
+++ src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const {PORT} = require('./config/serverConfig');
+
+const app = express();
+
+const setupAndStartServer = () => {
+    app.listen(PORT, () => {
+        console.log(`Server started at ${PORT}`);
+    });
+}
+
+setupAndStartServer();
