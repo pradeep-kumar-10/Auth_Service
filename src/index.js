d64624c98d39b839db9b208d2e38247617dfed70
Pradeep Kumar <pra45516@gmail.com>
2024-05-20 14:39:26 +0530

    Added jwt create and verify token logic

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const apiRoutes = require('./routes/index');
 
 const app = express();
 
-// const UserRepository = require('./repository/user-repository');
+// const UserService = require('./services/user-service');
 
 const setupAndStartServer = async() => {
 
@@ -18,10 +18,13 @@ const setupAndStartServer = async() => {
         console.log(`Server started at ${PORT}`);
     });
 
-    // const userRepo = new UserRepository();
-    // const response = await userRepo.getUserById(3);
+    // const userService = new UserService();
+    // const newToken = await userService.createToken({email: 'pradeep@admin.com', id: 3})
+    // console.log('new token is', newToken);
+    // const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InByYWRlZXBAYWRtaW4uY29tIiwiaWQiOjMsImlhdCI6MTcxNjE5NTg3NywiZXhwIjoxNzE2MjgyMjc3fQ.CeDoJO7IfO3y3W3j_VCA75b4enjfYfhrfBi2h-gQ-D0';
+    // const response = userService.verifyToken(token);
     // console.log(response);
-
+    
 }
 
 setupAndStartServer();
