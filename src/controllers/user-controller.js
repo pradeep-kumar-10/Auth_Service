be9d2da6bcd0d5381e2bf539ded717e046574f8b
Pradeep Kumar <pra45516@gmail.com>
2024-05-20 18:43:44 +0530

    Added Validation Error class

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -17,11 +17,11 @@ const createUser = async (req, res) => {
 
     } catch(error){
         console.log(error);
-        return res.status(500).json({
+        return res.status(error.statusCode).json({
             success: false,
             data: {},
-            message: 'Something went wrong',
-            err: error
+            message: error.message,
+            err: error.explanation
         });
     }
 }
